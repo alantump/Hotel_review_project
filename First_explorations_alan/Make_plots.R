f9dbd41f110d6ab9f33560f1e401d2512a96cef5
alantump <tump@mpib-berlin.mpg.de>
2024-12-15 16:52:29 +0100

    plots

--- First_explorations_alan/Make_plots.R
+++ First_explorations_alan/Make_plots.R
@@ -1,6 +1,6 @@
 source("functions.R")
 
-Hotel_Reviews <- data_loader("keggle")
+Hotel_Reviews <- data_loader("crete")
 
 library(re)
 gsub("[^0-9]", "", Hotel_Reviews$price[1])
@@ -15,12 +15,93 @@ Hotel_Reviews %>% group_by(Hotel_Name)   %>% summarise(m=mean(num_price), m_scor
 tt = Hotel_Reviews %>% group_by(Hotel_Name) %>% summarise(m=mean(num_price), m_score= mean(Reviewer_Score), n =n())  %>% filter(m<1000, n>100)
 lm(m ~ m_score, data=tt )
 
-# Filter hotels with at least 500 reviews
-Hotel_Reviews <- Hotel_Reviews %>%
-  group_by(Hotel_Name) %>%
-  filter(n() >= 200) %>% #, Country=="United Kingdom"
-  ungroup()
-min_number = 100
+
+
+
+
+##
+
+
+
+library(prophet)
+# Fit the model
+m <- prophet(Hotel_Reviews %>% mutate(ds = date_object, y =Reviewer_Score) ,changepoint.prior.scale = 0.0005,
+             yearly.seasonality=TRUE)
+
+# Make future dataframe for predictions
+#future <- make_future_dataframe(m, periods = trip_time) # Predict for the next 30 days
+
+# Forecast
+forecast <- predict(m, Hotel_Reviews %>% mutate(ds = date_object, y =Reviewer_Score)  )
+
+Hotel_Reviews = Hotel_Reviews %>% mutate(cleaned_Review = Reviewer_Score + forecast$weekly + forecast$yearly)
+
+#prophet_plot_components(m, forecast)
+
+
+
+
+
+#### Make example future plot
+
+#5   8  10  11  24  27  51  61  66 105 107 145 146 165 170 177 178 181 185
+
+i=unique(Hotel_Reviews$Hotel_Name)[178]
+#dim(Hotel_Reviews %>% filter(Hotel_Name==i) )
+
+singel_Hotel_Reviews2 = Hotel_Reviews %>% filter(Hotel_Name==i) %>% mutate(New =ifelse(date_object>=(max(date_object) - (356/2)),"Future", "Past"))
+
+singel_Hotel_Reviews_f = singel_Hotel_Reviews2 %>% filter(New=="Future") %>% mutate(average=mean(Reviewer_Score))
+singel_Hotel_Reviews_p = singel_Hotel_Reviews2 %>% filter(New=="Past") %>% mutate(average=mean(Reviewer_Score))#
+
+# Calculate the recency weights
+singel_Hotel_Reviews_p <- singel_Hotel_Reviews_p %>%
+  mutate(
+    # Calculate the difference in days from the most recent date
+    days_since = as.numeric(max(date_object) - date_object),
+    # Calculate weights using an exponential decay function
+    weight = exp(-days_since / (365 /4))  # Adjust the decay rate as needed
+  )
+recency_weighted_average <- with(singel_Hotel_Reviews_p, sum(Reviewer_Score * weight) / sum(weight))
+singel_Hotel_Reviews_p$recency_weighted_average = recency_weighted_average
+
+p = ggplot(singel_Hotel_Reviews2, aes(x = date_object, y = Reviewer_Score, color = New)) +
+  geom_point(alpha=0.2, size=0.8) +
+  geom_line(data=singel_Hotel_Reviews_f, aes(y=average, x = date_object), color= "black", size=1) +
+  geom_line(data=singel_Hotel_Reviews_p, aes(y=average, x = date_object), color = "#E69F00", size=1) +
+  #geom_line(data=singel_Hotel_Reviews_p, aes(y=recency_weighted_average, x = date_object), color="#56B4E9", size=2) +
+  #geom_smooth(method="lm") +
+  labs(#title = "Reviewer Scores Over Time",
+       x = "Date",
+       y = "Reviewer Score") +
+  coord_cartesian(ylim = c(6.5, 10)) + 
+  theme_minimal() + 
+  theme(legend.position = "top") +
+  scale_color_manual(name = "",values = c("black", "#E69F00"),
+                     labels = c("Future", "Past")) +
+  guides(color = guide_legend(override.aes = list(alpha = 1, size=2)))
+
+p 
+ggsave(paste("Data/hotel_average.png"),p, width =2.5, height = 2.5,dpi = 400)
+
+####
+
+# Calculate the recency weights
+p2 = expand.grid(days_old = seq(0,(356*2),10), decay = c(0.5,1,2)) %>%
+  mutate(
+    weight = exp(-days_old / (365/decay))  # Adjust the decay rate as needed
+  ) %>% ggplot(aes(y= weight, x=days_old, group=decay , color=factor(decay)) ) + geom_line(size=1.2) +   theme_minimal() +
+  ylab("Weight") + xlab("Rating age [days]") +
+  geom_vline(xintercept = 356, linetype ="dashed")+
+  labs(color = "Decay Rate")   + theme(legend.position = c(0.8, 0.7))
+
+p2
+ggsave(paste("Data/decay.png"),p2, width =3, height = 3,dpi = 400)
+
+
+
+##
+
 
 
 vec_pred=NULL
@@ -49,17 +130,20 @@ for(i in unique(Hotel_Reviews$Hotel_Name)){
       # Calculate the difference in days from the most recent date
       days_since = as.numeric(max(date_object) - date_object),
       # Calculate weights using an exponential decay function
-      weight = exp(-days_since / (365 /3))  # Adjust the decay rate as needed
+      weight = exp(-days_since / (365 /2.5))  # Adjust the decay rate as needed
     )
   
   # Calculate the recency-weighted average
   recency_weighted_average <- with(singel_Hotel_Reviews, sum(Reviewer_Score * weight) / sum(weight))
   m_fit = lm(Reviewer_Score ~ date_object, data = singel_Hotel_Reviews)
+  
   max_date <- max(singel_Hotel_Reviews$date_object, na.rm = TRUE)
   new_data <- data.frame(date_object = max_date)
   predicted_score <- predict(m_fit, newdata = new_data)
   mean_rating = round(mean(singel_Hotel_Reviews$Reviewer_Score),1)
   
+  
+
   vec_pred = c(vec_pred,mean_rating-predicted_score)
   vec_weight = c(vec_weight,mean_rating-recency_weighted_average)
   
@@ -70,13 +154,39 @@ mean((vec_weight)<(-0.2))
 mean((vec_weight)>(0.2))
 mean(abs(vec_weight)>(0.2))
 
+
+library(ggplot2)
+library(dplyr)
+
+test = data.frame(vec_weight, Name = unique(Hotel_Reviews$Hotel_Name), tendency = ifelse(abs(vec_weight) > 0.2, 1, 0)) %>%
+  arrange(vec_weight)
+
+test$Name <- factor(test$Name, levels = test$Name[order(test$vec_weight)])
+
+pp = test %>% ggplot(aes(x = Name, y = vec_weight, colour = as.factor(tendency))) +  geom_hline(yintercept = seq(-1,1,0.2), linetype="dashed", color="gray") +
+  coord_flip() +
+  geom_hline(yintercept = 0) +    theme_classic() +#theme_cowplot() +#theme_minimal_vgrid() +
+  geom_point() +
+  theme(legend.position = "None",
+        axis.text.y = element_blank(),  # Remove hotel names from y-axis
+        axis.ticks.y = element_blank()) + # Remove ticks along with labels
+  scale_color_manual(values = c("#F8766D", "#56B4E9", "#7CAE00")) +
+  annotate("text", x = Inf, y = Inf, label = paste("Positive trend:", round(mean(test$vec_weight > 0.2), 2)), hjust = 1.01, vjust = 2 + 10, size = 4, color = "#56B4E9") +
+  annotate("text", x = Inf, y = Inf, label = paste("No clear trend:", round(mean(test$tendency == 0), 2)), hjust = 1.01, vjust = 3.5 + 10, size = 4, color = "#F8766D") +
+  annotate("text", x = Inf, y = Inf, label = paste("Negative trend:", round(mean(test$vec_weight < (-0.2)), 2)), hjust = 1.01, vjust = 5 + 10, size = 4, color = "#56B4E9") +
+  xlab("Hotels of Crete") + ylab("Difference of \n simple and weighted average ratings") +
+  scale_y_continuous(breaks = seq(-1, 1, by = 0.5), limits = c(-1,1)) 
+pp
+ggsave(paste("Data/tendencies.png"),pp, width =3.6, height = 3.6,dpi = 400)
+
+
 # Calculate the recency weights
 singel_Hotel_Reviews %>%
   mutate(
     # Calculate the difference in days from the most recent date
     days_since = as.numeric(max(date_object) - date_object),
     # Calculate weights using an exponential decay function
-    weight = exp(-days_since / (365/3))  # Adjust the decay rate as needed
+    weight = exp(-days_since / (365/2))  # Adjust the decay rate as needed
   ) %>% ggplot(aes(y= weight, x=days_since )) + geom_point()
 
 
@@ -108,7 +218,7 @@ singel_Hotel_Reviews <- singel_Hotel_Reviews %>%
     # Calculate the difference in days from the most recent date
     days_since = as.numeric(max(date_object) - date_object),
     # Calculate weights using an exponential decay function
-    weight = exp(-days_since / 365 *2)  # Adjust the decay rate as needed
+    weight = exp(-days_since / (365 /2.5))  # Adjust the decay rate as needed
   )
 
 # Calculate the recency-weighted average
